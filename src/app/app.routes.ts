9990bd888ec651a2078ee7fb4f7d1cca286bdec4
DESKTOP-UAOJR3S\Jesús <jeusxe@gmail.com>
2025-06-14 12:01:21 +0200

    Basic header with navigation added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './features/about/about.component';
+import { LoginComponent } from './features/login/login.component';
+import { HomeComponent } from './features/home/home.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+    },
+    { path: 'about', component: AboutComponent },
+    { path: '**', redirectTo: 'login' }
+];
